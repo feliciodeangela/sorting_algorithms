9e32f9f8ac84ab6550d524f3344b01413695923d
felicio <felicio@localhost.localdomain>
2023-09-25 12:43:51 +0200

    up task 0, header and printers

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * bubble_sort - Entry point.
+ * Description: Sort an array of integers in ascending order using bubble sort.
+ * @array: Array (Data structure) to sort.
+ * @size: Number of elements in the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	
+}
